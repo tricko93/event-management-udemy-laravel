d3e4c5634de3b5d8a7e3ade5df028b91f76e47a1
Marko Trickovic <96424684+tricko93@users.noreply.github.com>
2023-12-08 23:20:53 +0100

    feature Add EventReminderNo.,update SendEventRem.

--- app/Console/Commands/SendEventReminders.php
+++ app/Console/Commands/SendEventReminders.php
@@ -38,7 +38,11 @@ class SendEventReminders extends Command
 
         $events->each(
             fn($event) => $event->attendees->each(
-                fn($attendee) => $this->info("Notifying the user {$attendee->user->id}")
+                fn($attendee) => $attendee->user->notify(
+                    new EventReminderNotification(
+                        $event
+                    )
+                )
             )
         );
 
